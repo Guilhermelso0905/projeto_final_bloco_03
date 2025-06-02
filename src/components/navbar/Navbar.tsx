620dc548fbe82488c9e79b94a8d171b1c13bac80
Guilherme Lima <guilhermelso2005@gmail.com>
2025-06-02 11:41:08 -0300

    adcionado as rotas

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -12,7 +12,7 @@ function Navbar() {
                             </div>
                             <div className="hidden sm:ml-6 sm:block">
                                 <div className="flex space-x-4">
-                                    <a href="#" className="block rounded-md bg-cyan-500 px-3 py-2 text-base font-medium text-white" aria-current="page">Farmacia Drogazen</a>
+                                    <a href="/home" className="block rounded-md bg-cyan-500 px-3 py-2 text-base font-medium text-white" aria-current="page">Farmacia Drogazen</a>
                                     <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-cyan-400 hover:text-white">Categorias</a>
                                     <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-cyan-400 hover:text-white">Nova Categoria</a>
                                 </div>
